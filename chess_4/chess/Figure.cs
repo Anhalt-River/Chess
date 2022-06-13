4590d3b6bce463843fbdf66caf24e7c2e39727d7
Bakirov Karim <85508867+Anhalt-River@users.noreply.github.com>
2022-06-13 19:16:59 +0300

    Add files via upload

---
+++ chess_4/chess/Figure.cs
@@ -0,0 +1,23 @@
+﻿namespace chess_4
+{
+    public partial class MainWindow
+    {
+        class Figure
+        {
+            public Point A;
+            public Point B;
+
+            public Figure(Point a, Point b)
+            {
+                A = a;
+                B = b;
+            }
+
+            public virtual bool Answer()
+            {
+                bool move = false;
+                return move;
+            }
+        }
+    }
+}
